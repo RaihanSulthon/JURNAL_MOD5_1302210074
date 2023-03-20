6bd16b502be36737912ea3dd20c68a17768c3629
Raihan Sulthon <raihansy@student.telkomuniversity.ac.id>
2023-03-20 07:48:22 +0700

    menambahkan class penjumlahan dengan method JumlahTigaAngka

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace JURNAL_MOD5_1302210074
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            Penjumlahan<double> hitung = new Penjumlahan<double>();
+            Console.WriteLine("Masukkan Angka: ");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+
+            hitung.JumlahTigaAngka(a, b, c);
+        }
+    }
+}
